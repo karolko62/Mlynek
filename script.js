938216e1d809697bf5487a456f330890ed505d27
karolko62 <karolko62@seznam.cz>
2026-09-20 22:32:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const start=new Date("2026-07-29T16:31:00+02:00");const el=document.getElementById("counter");function update(){const n=Math.max(0,Math.floor((Date.now()-start.getTime())/3000));el.textContent=n.toLocaleString("cs-CZ")}update();setInterval(update,1000);
+const start=new Date("2026-07-29T16:31:00+02:00");
+const el=document.getElementById("counter");
+function update(){
+  const n=Math.max(0,Math.floor((Date.now()-start.getTime())/3000));
+  el.textContent=n.toLocaleString("cs-CZ");
+}
+update();
+setInterval(update,1000);
